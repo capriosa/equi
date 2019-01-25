28523ddcb05debd82621efd35da52bfc49cdac08
Meinolf Droste <mdwp.post@gmail.com>
2019-01-25 13:41:04 +0100

    bugfix

---
+++ src/Widgets/AccordionWidget/AccordionWidgetComponent.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import * as Scrivito from 'scrivito';
+import InPlaceEditingPlaceholder from '../../Components/InPlaceEditingPlaceholder';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+} from 'react-accessible-accordion';
+
+
+
+class AccordionComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
+    const widget = this.props.widget;
+    const items = widget.get('items');
+
+
+
+    if (!items.length) {
+      return (
+        <InPlaceEditingPlaceholder center={ true }>
+          Create Accordion Items in widget properties.
+        </InPlaceEditingPlaceholder>
+      );
+    }
+
+const list = items.map((item, index) =>
+
+<AccordionItem key={index}>
+
+
+
+
+    <AccordionItemTitle>
+      <div className='row'>
+
+      <div className={`col col-md-3 item-${index}`}>
+    <Scrivito.ImageTag content={ item.get('icon') }  />
+    </div>
+    <div className='col col-md-9'>
+        <h3>{item.get('title')}</h3>
+        <div>{item.get('teaser')}</div>
+      </div>
+      </div>
+    </AccordionItemTitle>
+    <AccordionItemBody>
+      <div className='row'>
+      <div className='col col-md-3'>
+      <Scrivito.ImageTag content={ item.get('image') }  />
+      </div>
+      <div className='col col-md-9'>
+        <p>{item.get('text')}</p>
+      </div>
+      </div>
+    </AccordionItemBody>
+
+
+</AccordionItem>
+
+
+
+);
+
+    return (
+    <Accordion>
+        {list}
+    </Accordion>
+
+    );
+}
+}
+
+Scrivito.provideComponent('AccordionWidget', AccordionComponent);
+
+
