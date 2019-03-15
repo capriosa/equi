350798d1f672c0edba82896411c5ccb5378bf364
Meinolf Droste <mdwp.post@gmail.com>
2019-03-15 11:10:24 +0100

    mapbox 0.70

--- src/Widgets/MapboxWidget/MapboxWidgetComponent.js
+++ src/Widgets/MapboxWidget/MapboxWidgetComponent.js
@@ -9,7 +9,7 @@ console.log(kundenliste)
   return (
 
     <div className="map">
-        <EquiMap mapboxApiAccessToken="pk.eyJ1IjoianVsaWFuczM3IiwiYSI6ImNpcDdmNWdlbDAwMmR2M25sa3k0Yjd0bG8ifQ.2S1SPdEJLAffhv-a0HEMIQ" kundenliste={kundenliste} fitBounds />
+        <EquiMap overlayText="Nutze 2 Finger zum Zoomen" mapboxApiAccessToken="pk.eyJ1IjoianVsaWFuczM3IiwiYSI6ImNpcDdmNWdlbDAwMmR2M25sa3k0Yjd0bG8ifQ.2S1SPdEJLAffhv-a0HEMIQ" kundenliste={kundenliste} fitBounds />
     </div>
 
   )
