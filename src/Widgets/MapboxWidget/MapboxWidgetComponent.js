3860a16aab1b49c63c614ddc000f2073dcb7e7cb
Meinolf Droste <mdwp.post@gmail.com>
2019-03-17 11:19:54 +0100

    map css

--- src/Widgets/MapboxWidget/MapboxWidgetComponent.js
+++ src/Widgets/MapboxWidget/MapboxWidgetComponent.js
@@ -2,14 +2,15 @@ import * as React from "react";
 import * as Scrivito from "scrivito";
 import { EquiMap } from "mulando-equimap";
 import * as kundenliste from "./kundenliste.csv";
+import "mulando-equimap/build/lib/EquiMap/EquiMap.css";
 
 
 Scrivito.provideComponent("MapboxWidget", ({ widget }) => {
-console.log(kundenliste)
+
   return (
 
     <div className="map">
-        <EquiMap overlayText="Nutze 2 Finger zum Zoomen" mapboxApiAccessToken="pk.eyJ1IjoianVsaWFuczM3IiwiYSI6ImNpcDdmNWdlbDAwMmR2M25sa3k0Yjd0bG8ifQ.2S1SPdEJLAffhv-a0HEMIQ" kundenliste={kundenliste} fitBounds />
+        <EquiMap mapboxApiAccessToken="pk.eyJ1IjoianVsaWFuczM3IiwiYSI6ImNpcDdmNWdlbDAwMmR2M25sa3k0Yjd0bG8ifQ.2S1SPdEJLAffhv-a0HEMIQ" kundenliste={kundenliste} fitBounds />
     </div>
 
   )
